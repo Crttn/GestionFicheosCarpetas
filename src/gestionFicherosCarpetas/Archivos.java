cab216a446f3b9e59ef2f39ce051740c28c09971
Cristian G <crttngr@gmail.com>
2024-04-29 09:11:57 +0100

    first development

--- src/gestionFicherosCarpetas/Archivos.java
+++ src/gestionFicherosCarpetas/Archivos.java
@@ -2,18 +2,21 @@ package gestionFicherosCarpetas;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Archivos {
 	
+	// Lista donde se almacenan todos los datos del archivo de nulidad
 	private static ArrayList<String[]> archivos = new ArrayList<>();
 	
+	// Carga los datos del arvhico de nulidad en el arrayList para su mejor manejo
 	public static void cargarArchivo(String nombre) {
 		archivos.clear();
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(nombre + ".txt"));
 			String linea;
 			while ((linea = br.readLine()) != null) {
-				String[] datoArchivo = linea.split(" ");
+				String[] datoArchivo = linea.split("	");
 				archivos.add(datoArchivo);
 			}
 			br.close();
@@ -22,20 +25,36 @@ public class Archivos {
 		}
 	}
 	
+	public static void generarNulidad(Scanner sc) {
+		String path = Estilos.solicitarTexto(sc, "Introduce la ruta en la que se generarán las nulidades: ");
+	}
+	
+	// Obtener todos los registros del archivo
 	public static void leerArchivo() {
 		for (String[] archivo : archivos) {
 			for (int i = 0; i < archivo.length; i++) {
-				System.out.print(archivo[i]);
-				// Añadir espacios para alinear los datos
-				for (int j = 0; j < archivo[i].length() + 4; j++) {
-					System.out.print(" ");
-				}
+				System.out.print(archivo[i] + "	");
 			}
 			System.out.println();
 		}
 	}
 	
-	// Crear archivo de nulidad a partir de un nombre
+	// Obtener un valor a partir del id de pago
+	public static void obtenerNulidad(String id) {
+		for (String[] archivo : archivos) {
+			if (archivo[0].equals(id)) {
+				System.out.println(archivo[8]);
+			}
+		}
+	}
+	
+	public static void crearArchivos() {
+		for (int i = 0; i < archivos.size(); i++) {
+			System.out.println(i++);
+		}
+	}
+	
+	// Crear archivo de nulidad a partir de un nombre || Generar los correos 
 	public static void crearArchivo(String nombre) {
 		File archivo;
 		archivo = new File(nombre + ".txt");
